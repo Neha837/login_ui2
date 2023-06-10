b8e8b846eb740d2d54a8f5b826eec6ddc270c1de
neha shahid <nehashahid293@gmail.com>
2023-06-10 11:35:43 +0500

    commit1

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          home: LoginPage(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff383B42),
+      body: Container(
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Image.asset(
+                "images/pic.PNG",
+                height: 45.h,
+                width: 100.w,
+              ),
+            ]),
+            Container(
+              height: 55.h,
+              width: 100.w,
+              decoration: BoxDecoration(
+                color: Color(0xff1F2126),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.all(25)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 8.h,
+                        width: 90.w,
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Color(0xff31D8E3),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: Text(
+                          "Sign Up with Email",
+                          style: TextStyle(
+                              color: Color(
+                                0xffffffff,
+                              ),
+                              fontSize: 16.sp),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 3.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Or Sign Up with",
+                        style: TextStyle(
+                            fontSize: 14.sp, color: Color(0xffFFFFFF)),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 3.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 8.h,
+                            width: 27.w,
+                            child: Image.asset(
+                              "images/Vector.png",
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color(0xff1F2126),
+                              border: Border.all(color: Color(0xff383B42)),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 3.w,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 8.h,
+                            width: 27.w,
+                            child: Image.asset("images/twitter.png"),
+                            decoration: BoxDecoration(
+                              color: Color(0xff1F2126),
+                              border: Border.all(color: Color(0xff383B42)),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 3.w,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 8.h,
+                            width: 27.w,
+                            child: Image.asset("images/Googleicon.png"),
+                            decoration: BoxDecoration(
+                              color: Color(0xff1F2126),
+                              border: Border.all(color: Color(0xff383B42)),
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 6.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                            color: Color(0xffFFFFFF), fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        width: 1.w,
+                      ),
+                      Text(
+                        "Sign in now",
+                        style: TextStyle(
+                            color: Color(0xff5385E5),
+                            fontSize: 15.sp,
+                            decoration: TextDecoration.underline),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
